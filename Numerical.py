986acbc71c85011583e2881d33b6eefeb4817fbd
Daniel Lengyel <dl2119@marne.doc.ic.ac.uk>
2020-01-28 20:23:20 +0000

    testing the numerics for the diff eq

--- Numerical.py
+++ Numerical.py
@@ -8,7 +8,7 @@ from Functions import Gibbs, GradGibbs
 
 def one_d_feynman_kac(process):
     u_start = process["u_start"]
-    x_low, x_high, t_low, t_high = process["domain"] # closed interval for spatial and time
+    x_low, x_high, t_low, t_highfor  = process["domain"] # closed interval for spatial
     num_x, num_t = process["num_x"], process["num_y"] # number of points to approximate
     sig = process["temperature"]
 
@@ -21,27 +21,15 @@ def one_d_feynman_kac(process):
     # init u
     u = np.array([[u_start(x) for x in range(x_low, x_high)]]) # 1D is time and rest are space
 
+    V = lambda x: np.linalg.norm(grad_U(x), axis=0)
+
     # create forward matrix
     forward_matrix = []
     for i in range(num_x):
-        row = [1/(4 * delta_x**2), grad_U(delta_x * i)/(2 * delta_x), - sig**2/2. * 1/(2 * delta_x ** 2) - V(delta_x * i) + 1,
-         b(delta_x * i)/(2 * delta_x), sig**2 / 2. * 1/(4. * delta_x)]
+        row = [1/(4 * delta_x**2), grad_U(np.array([[delta_x * i]]))[0][0]/(2 * delta_x), - sig**2/2. * 1/(2. * delta_x ** 2) - V(delta_x * i)[0] + 1,
+         grad_U(delta_x * i)[0][0]/(2 * delta_x), sig**2 / 2. * 1/(4. * delta_x)]
+        forward_matrix.append(np.concatenate([a, row, b]))
 
-        \frac
-        {1}
-        {4 \delta_x ^ 2} & & - \frac
-        {b(x_j)}
-        {2 *\delta_x} & & -\frac
-        {\sigma ^ 2}{2} \frac
-        {1}
-        {2 \delta_x ^ 2} - \frac
-        {V(x_j) + 1}
-        {1} & & \frac
-        {b(x_j)}
-        {2 *\delta_x} & &  \frac
-        {\sigma ^ 2}{2} \frac
-        {1}
-        {4 \delta_x ^ 2} & & 0 & & \dots
 
 
     for t in range(t_low, t_high+1):
