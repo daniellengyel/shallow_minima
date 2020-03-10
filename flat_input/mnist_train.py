42d052a86d62c1d222fd29f786947f9a33c3f57e
Daniel Lengyel <daniel.lengyel@berkeley.edu>
2020-03-10 01:25:12 +0000

    added logging features and fixed the sampling algorithm for nn's. also did some feature exploration

---
+++ flat_input/mnist_train.py
@@ -0,0 +1,155 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import torch
+import torchvision
+import torch.optim as optim
+
+from torch.utils.tensorboard import SummaryWriter
+
+from utils import *
+from nets.LeNet import LeNet
+
+def get_data(config):
+    train_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST('./data/', train=True, download=True,
+                                   transform=torchvision.transforms.Compose([
+                                       torchvision.transforms.ToTensor(),
+                                       torchvision.transforms.Normalize(
+                                           (0.1307,), (0.3081,))
+                                   ])),
+        batch_size=config["batch_size"]["train_size"], shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST('./data/', train=False, download=True,
+                                   transform=torchvision.transforms.Compose([
+                                       torchvision.transforms.ToTensor(),
+                                       torchvision.transforms.Normalize(
+                                           (0.1307,), (0.3081,))
+                                   ])),
+        batch_size=config["batch_size"]["test_size"], shuffle=True)
+
+    return train_loader, test_loader
+
+
+def train(config):
+    # init torch
+    torch.backends.cudnn.enabled = False
+    if config["torch_random_seed"] is not None:
+        torch.manual_seed(config["torch_random_seed"])
+
+    # get data
+    train_loader, test_loader = get_data(config)
+
+
+    # Init neural nets and weights
+    num_nets = config["num_nets"]
+    nets = [LeNet() for _ in range(num_nets)]
+    nets_weights = np.zeros(num_nets)
+
+    #  Define a Loss function and optimizer
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizers = [optim.SGD(nets[i].parameters(), lr=config["SGD_params"]["learning_rate"],
+                            momentum=config["SGD_params"]["momentum"]) for i in range(num_nets)]
+
+    # Set algorithm params
+    weight_type = "input_output_forbenius"
+
+    # tau = 2500  # num batches before resampling
+    beta = -40
+
+    writer = SummaryWriter()
+
+    # get train loaders for each net
+    net_data_loaders = [iter(enumerate(train_loader, 0)) for _ in range(num_nets)]
+
+    # train
+    for epoch in range(config["num_epochs"]):
+        is_training_epoch = True
+        while is_training_epoch:
+            for idx_net in range(num_nets):
+                # get net and optimizer
+                net = nets[idx_net]
+                optimizer = optimizers[idx_net]
+
+                # get the inputs; data is a list of [inputs, labels]
+                try:
+                    i, data = next(net_data_loaders[idx_net])
+                except:
+                    is_training_epoch = False
+                    break
+                inputs, labels = data
+                labels = labels.reshape(len(labels)).long()
+
+                # Compute gradients for input.
+                inputs.requires_grad = True
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = net(inputs)
+                loss = criterion(outputs.float(), labels)
+                loss.backward(retain_graph=True)
+                optimizer.step()
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # get input gradients
+                output_forb = torch.norm(outputs)
+                output_forb.backward()
+                input_grads = inputs.grad
+
+                # update weights
+                if weight_type == "input_output_forbenius":
+                    curr_weight = weight_function(input_grads)
+                    nets_weights[idx_net] += curr_weight
+                elif weight_type == "loss_gradient_weights":
+                    pass
+                else:
+                    raise NotImplementedError()
+
+                # store training accuracy current
+                writer.add_scalar('Loss/train/net {}'.format(idx_net), loss, i + epoch*len(train_loader))
+
+                writer.add_scalar('Weight/net {}'.format(idx_net), curr_weight, i + epoch*len(train_loader))
+
+
+            writer.add_scalar('Kish/', kish_effs(nets_weights), i + epoch*len(train_loader))
+
+            # Check resample
+            if kish_effs(nets_weights) < 0.95:
+                # resample particles
+                sampled_idx = sample_index_softmax(nets_weights, nets, beta=beta)
+                # init nets etc
+                nets = [copy.deepcopy(nets[i]) for i in sampled_idx]
+                optimizers = [optim.SGD(nets[i].parameters(), lr=config["SGD_params"]["learning_rate"],
+                                        momentum=config["SGD_params"]["momentum"]) for i in range(num_nets)]
+                nets_weights = np.zeros(num_nets)
+                print(sampled_idx)
+
+        # get test error
+        correct = 0
+        _sum = 0
+
+        for idx, (test_x, test_label) in enumerate(test_loader):
+            predict_y = nets[0](test_x.float()).detach()
+            predict_ys = np.argmax(predict_y, axis=-1)
+            label_np = test_label.numpy()
+            _ = predict_ys == test_label
+            correct += np.sum(_.numpy(), axis=-1)
+            _sum += _.shape[0]
+
+        print('accuracy: {:.2f}'.format(correct / _sum))
+        torch.save(nets[0], 'models/mnist_{:.2f}.pkl'.format(correct / _sum))
+
+        # get train loaders for each net
+        net_data_loaders = [iter(enumerate(train_loader, 0)) for _ in range(num_nets)]
+
+
+
+    return nets
+
+
+
--- flat_input/utils.py
+++ flat_input/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# algorithm 
 
 def sample_index_softmax(weights, positions, beta=1):
     probabilities = softmax(weights, beta)
@@ -16,8 +17,102 @@ def softmax(weights, beta=-1):
     return probabilities
 
 
-def weight_function(grad, curr_weight):
+def weight_function(grad):
     input_shape = grad.shape  # batch, filters, x_dim, y_dim
     grad = grad.reshape((input_shape[0], np.product(input_shape[1:]))).T
 
-    return curr_weight + np.sum(np.linalg.norm(grad, axis=0))
+    return np.sum(np.linalg.norm(grad, axis=0))
+
+
+def kish_effs(weights):
+    """Assume weights are just a list of numbers"""
+    N = len(weights)
+    weights = np.array(weights)
+    sum_weights = np.sum(weights)
+    return 1/float(N) *  sum_weights**2 / weights.dot(weights)
+
+
+# Viz 
+
+def classification_regions_2d(v1, v2, center_image, alpha_min, alpha_max, beta_min, beta_max, N, net):
+    """ 
+    Returns the alpha (X) and beta (Y) range used in the basis of v1 and v2. Use meshgrid(X, Y) to get the corresponding 
+    coordinates for the result. """
+    
+    
+    alpha_range = np.linspace(alpha_min, alpha_max, N)
+    beta_range = np.linspace(beta_min, beta_max, N)
+    
+    results = []
+    
+    mesh = np.array(np.meshgrid(alpha_range, beta_range))
+    
+    mesh_2d = mesh.reshape(2, N*N)
+    
+    max_batch = 250*250
+    i = 1
+    results = np.array([])
+    
+    net.eval()
+    
+    while N*N > max_batch*(i - 1): 
+        lin_comb = torch.stack([v1, v2]).T.mm(torch.Tensor(mesh_2d[:, (i-1)*max_batch:i*max_batch])).T
+        lin_comb = lin_comb.reshape(lin_comb.shape[0], 1, center_image.shape[1], center_image.shape[2])
+        lin_comb += center_image
+        curr_results = net(lin_comb)
+        curr_results = torch.argmax(curr_results, 1).detach().numpy()
+        
+        results = np.concatenate([results, curr_results])
+        i += 1
+        
+    mesh = np.array(np.meshgrid(alpha_range, beta_range))
+    return alpha_range, beta_range, np.array(results).reshape(mesh.shape[1:]), v1, v2
+
+# from: https://github.com/facebookresearch/jacobian_regularizer/blob/master/jacobian/jacobian.py
+def _random_vector(C, B):
+    '''
+    creates a random vector of dimension C with a norm of C^(1/2)
+    (as needed for the projection formula to work)
+    '''
+    if C == 1: 
+        return torch.ones(B)
+    v=torch.randn(B,C)
+    arxilirary_zero=torch.zeros(B,C)
+    vnorm=torch.norm(v, 2, 1,True)
+    v=torch.addcdiv(arxilirary_zero, 1.0, v, vnorm)
+    return v
+
+
+def get_vec(img):
+    return img.reshape(np.product(img.shape[1:]))
+
+def get_orthogonal_basis(img1, img2, img3):
+    """img1 is center. img_2 is first orthogonal vector. use graham schmidt to get second vector from img_3."""
+    img1_to_img2 = get_vec(img2) - get_vec(img1)
+    unit_img1_to_img2 = img1_to_img2 / np.linalg.norm(img1_to_img2)
+    
+    img1_to_img3 = get_vec(img3) - get_vec(img1)
+    unit_img1_to_img3 = img1_to_img3 / np.linalg.norm(img1_to_img3)
+    
+    assert abs(unit_img1_to_img2.dot(unit_img1_to_img3)) != 1
+    
+    # get orthogonal vectors which span the above subspace. Use grahamschmidt
+    v1 = unit_img1_to_img2
+    v2 = unit_img1_to_img3 - v1.dot(unit_img1_to_img3) * v1
+    v2 = v2 / np.linalg.norm(v2)
+    
+    return v1, v2
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# net predictions
+def get_average_output(nets, inp):
+    outs = [net(inp).detach().numpy() for net in nets]
+    return np.mean(outs, axis=0)
+    
+
